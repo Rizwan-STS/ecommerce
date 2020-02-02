f89937c06c48264905152561964eff23a17c304a
Rizwan Malvat <parentsgift@Rizwans-Mac-mini.local>
2020-02-02 21:29:13 +0530

    Design Updates

--- src/app/Add_Payment_Method/Add_Payment_Method.component.ts
+++ src/app/Add_Payment_Method/Add_Payment_Method.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/app.service';
 
+declare var $: any;
+
 @Component({
   selector: 'app-Add_Payment_Method',
   templateUrl: './Add_Payment_Method.component.html',
@@ -11,6 +13,9 @@ export class AddPaymentMethodComponent implements OnInit {
   constructor(private appService: AppService) { }
 
   ngOnInit() {
+    setTimeout(() => {
+      $('#myModal1').modal("show")
+  }, 1000)
   }
 
 }
