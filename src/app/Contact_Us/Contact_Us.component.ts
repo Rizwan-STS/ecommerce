f761b53c8397f53baca67616cf1b823716f3e557
Viral Pattani <pattaniviralb@gmail.com>
2020-02-03 01:55:25 +0530

    contact us with api integration product description with api integration list product navigate to product details

--- src/app/Contact_Us/Contact_Us.component.ts
+++ src/app/Contact_Us/Contact_Us.component.ts
@@ -1,16 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { AppService } from 'src/app/app.service';
+import {Component, OnInit} from '@angular/core';
+import {AppService} from 'src/app/app.service';
+import {environment} from "../../environments/environment";
+declare var $: any;
 
 @Component({
-  selector: 'app-Contact_Us',
-  templateUrl: './Contact_Us.component.html',
-  styleUrls: ['./Contact_Us.component.scss']
+    selector: 'app-Contact_Us',
+    templateUrl: './Contact_Us.component.html',
+    styleUrls: ['./Contact_Us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
+    email = '';
+    title = '';
+    message = '';
 
-  constructor(private appService: AppService) { }
+    constructor(private appService: AppService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    ValidateEmail(mail) {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+            return (true)
+        }
+        return (false)
+    }
+
+    SendInquiry() {
+        if (this.email && this.title && this.message) {
+            if (!this.ValidateEmail(this.email)) {
+                alert('You have entered an invalid email address.')
+                $('#email').focus();
+                return;
+            }
+            this.appService.postMethod(environment.baseUrl + '/contact-us', [], [], [], {
+                email: this.email,
+                title: this.title,
+                comment: this.message
+            }).subscribe((data: any) => {
+                alert(data.message);
+                this.title = '';
+                this.email = '';
+                this.message = '';
+            }, err => {
+                console.log(err);
+                this.title = '';
+                this.email = '';
+                this.message = '';
+            });
+        } else {
+            alert('All fields are required.')
+        }
+    }
 
 }
--- src/app/Product_Detail/Product_Detail.component.ts
+++ src/app/Product_Detail/Product_Detail.component.ts
@@ -1,16 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { AppService } from 'src/app/app.service';
+import {Component, OnInit} from '@angular/core';
+import {AppService} from 'src/app/app.service';
+import {ActivatedRoute, Router} from "@angular/router";
+declare var $: any;
 
 @Component({
-  selector: 'app-Product_Detail',
-  templateUrl: './Product_Detail.component.html',
-  styleUrls: ['./Product_Detail.component.scss']
+    selector: 'app-Product_Detail',
+    templateUrl: './Product_Detail.component.html',
+    styleUrls: ['./Product_Detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
+    productDetails = null;
+    productYoMayLikeDetails= [];
+    selectedProduct = null;
 
-  constructor(private appService: AppService) { }
+    constructor(private appService: AppService, private route: ActivatedRoute) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.route.data.subscribe((data) => {
+            this.productDetails = data.ProductDetails[0].data;
+            this.productYoMayLikeDetails = data.ProductDetails[1].data.adminBoxProductsResponseModels;
+            debugger
+            this.selectImage(this.productDetails.adminProductsResponseModels.productsImagesResponseModel[0]);
+        });
+    }
+
+    AddProductQt(product) {
+        if (product.adminProductsResponseModels.prQuantity) {
+            let qty = +(product.adminProductsResponseModels.prQuantity);
+            qty += 1
+            product.adminProductsResponseModels.prQuantity = qty;
+        } else {
+            product.adminProductsResponseModels.prQuantity = 1
+        }
+    }
+
+    MinusProductQt(product) {
+        if (product.adminProductsResponseModels.prQuantity) {
+            let qty = +(product.adminProductsResponseModels.prQuantity);
+            qty -= 1
+            qty = qty < 0 ? 0 : qty;
+            product.adminProductsResponseModels.prQuantity = qty;
+        } else {
+            product.adminProductsResponseModels.prQuantity = 0
+        }
+    }
+
+    selectImage(img) {
+        for (let image of this.productDetails.adminProductsResponseModels.productsImagesResponseModel) {
+            image.selected = false;
+        }
+        img.selected = true;
+    }
+
+    AddtoCart(p) {
+        this.selectedProduct = p;
+        $('#myModal1').modal("show")
+    }
 
 }
